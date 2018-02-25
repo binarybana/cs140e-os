e260483c1707d89d313e993dfe0a66f25c7d3797
Jason Knight <Jason@JasonKnight.us>
2018-02-24 22:51:32 -0800

    Mostly working shell, broken echo

--- kernel/src/kmain.rs
+++ kernel/src/kmain.rs
@@ -46,6 +46,7 @@ pub fn blink(times: u8, interval: u64) {
 pub fn echo() {
     use pi::timer::spin_sleep_ms;
     use pi::uart::MiniUart;
+    use std::fmt::Write;
 
     let mut uart = MiniUart::new();
 
@@ -56,11 +57,29 @@ pub fn echo() {
     }
 }
 
+pub fn shell() {
+    use console::{kprint, kprintln};
+    use std::fmt::Write;
+    use pi::timer::spin_sleep_ms;
+    loop {
+        kprintln!("Hello world");
+        kprint!("Hello to ");
+        kprint!("you too!");
+        spin_sleep_ms(200);
+    }
+    // let mut console = console::CONSOLE.lock();
+    // loop {
+    //     console.write_str("\n<-");
+    //     let b = console.read_byte();
+    //     console.write_byte(b);
+    //     // kprintln!("$ {}", b);
+    // }
+}
+
 #[no_mangle]
 pub extern "C" fn kmain() {
     // blink(3, 1000);
-
-
-    echo();
-    // FIXME: Start the shell.
+    // echo();
+    // shell();
+    shell::shell("> ");
 }
--- kernel/src/shell.rs
+++ kernel/src/shell.rs
@@ -36,12 +36,64 @@ impl<'a> Command<'a> {
 
     /// Returns this command's path. This is equivalent to the first argument.
     fn path(&self) -> &str {
-        unimplemented!()
+        self.args[0]
     }
+}
+
+const BS: u8 = 0x08;
+const BEL: u8 = 0x07;
+const LF: u8 = 0x0A;
+const CR: u8 = 0x0D;
+const DEL: u8 = 0x7F;
+
+/// Parse an input command line and deal with bells etc.
+/// Here I started to use vincenthouyi's repo and code for heavy inspiration
+fn readline(buf: &mut [u8]) -> &str {
+    let mut read = 0;
+    loop {
+        let b = CONSOLE.lock().read_byte();
+        match b {
+            BS | DEL if read > 0 => {
+                read -= 1;
+                kprint!("{}", BS as char);
+                kprint!(" ");
+                kprint!("{}", BS as char);
+            }
+            LF | CR => {
+                kprintln!();
+                break;
+            }
+            _ if read == buf.len() => {
+                kprint!("{}", BEL as char);
+            }
+            byte @ b' '... b'~' => {
+                buf[read] = byte;
+                read += 1;
+                kprint!("{}", byte as char);
+            }
+            _ => kprint!("{}", BEL as char),
+        }
+    }
+    use std::str;
+    return str::from_utf8(&buf[..read]).unwrap();
 }
 
 /// Starts a shell using `prefix` as the prefix for each line. This function
 /// never returns: it is perpetually in a shell loop.
 pub fn shell(prefix: &str) -> ! {
-    unimplemented!()
+    let mut buf = [0u8; 512];
+    loop {
+        kprint!("{}", prefix);
+        let cmd = readline(&mut buf);
+        match Command::parse(cmd, &mut [""; 64]) {
+            Err(Error::TooManyArgs) => kprintln!("Too many arguments"),
+            Err(Error::Empty) => { }
+            Ok(cmd) => {
+                match cmd.path() {
+                    "echo" => kprintln!("{}", cmd.args[1]),
+                    v => kprintln!("unknown command: {}", v),
+                }
+            }
+        }
+    }
 }
